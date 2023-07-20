177147ba596d03406bba81abb8cce36ef888a2a0
argg <argg9822@gmail.com>
2023-07-19 20:03:45 -0500

    Implementacion React upd 3

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
 import logo from './logo.png';
-import './App.css';
+import './styles/css-login.css';
 
 function App() {
+  let hora = new Date().toLocaleTimeString();
+  let size_logo = 300;
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} alt="logo" />
-        <p>
-          Nuevas páginas
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={logo} alt="logo" width={size_logo}/>
+        <h1>Nuevas páginas</h1>
+        <input type = "text" placeholder = "Usuario" className='input-login'></input>
+        <input type = "password" placeholder = "Contraseña" className='input-login'></input>
+        <div>{hora}</div>
       </header>
     </div>
   );

a262ddcee04af1bf838962211d5bbc554bc1d679
argg9822 <argg9822@gmail.com>
2023-07-20 12:43:43 -0500

    Apariencia Login

--- src/App.js
+++ src/App.js
@@ -2,17 +2,23 @@ import logo from './logo.png';
 import './styles/css-login.css';
 
 function App() {
-  
-  let size_logo = 250;
+  let size_logo = 200;
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} alt="logo" width={size_logo}/>
-        <h1 className='color'>Nuevas páginas</h1>
-        <input type = "text" placeholder = "Usuario" className='input-login'></input>
-        <input type = "password" placeholder = "Contraseña" className='input-login'></input>
-        <button type='submit' className='btn-login'>Ingresar</button>
+      <header className='App-header align-right'>
+        <a href='https://gesiapp.com.co/'>Registrarse</a>
       </header>
+      <section className="App-section">
+        <div className='box-logo'>
+          <img src={logo} alt="logo" width={size_logo}/>
+        </div>
+        <div className='flex-column flex-center w-100 box-login'>
+          <h1>Nuevas páginas</h1>
+          <input type = "text" placeholder = "Usuario" className='input-login'></input>
+          <input type = "password" placeholder = "Contraseña" className='input-login'></input>
+          <button type='submit' className='btn-login'>Ingresar</button>
+        </div>
+      </section>
     </div>
   );
 }

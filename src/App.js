229212220a3db1ccac3d8473070f7c0b9d7953a4
argg9822 <argg9822@gmail.com>
2023-07-19 21:51:54 -0500

    Implementacion React upd 4

--- src/App.js
+++ src/App.js
@@ -2,16 +2,16 @@ import logo from './logo.png';
 import './styles/css-login.css';
 
 function App() {
-  let hora = new Date().toLocaleTimeString();
-  let size_logo = 300;
+  
+  let size_logo = 250;
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} alt="logo" width={size_logo}/>
-        <h1>Nuevas páginas</h1>
+        <h1 className='color'>Nuevas páginas</h1>
         <input type = "text" placeholder = "Usuario" className='input-login'></input>
         <input type = "password" placeholder = "Contraseña" className='input-login'></input>
-        <div>{hora}</div>
+        <button type='submit' className='btn-login'>Ingresar</button>
       </header>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -11,6 +11,16 @@ root.render(
   </React.StrictMode>
 );
 
+let hora = new Date().toLocaleTimeString();
+
+const elementNow = ReactDOM.createRoot(document.getElementById('hour'));
+elementNow.render(
+  <span>{hora}</span>
+)
+
+
+//ReactDOM.render(elementNow, document.getElementById('hour'));
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
